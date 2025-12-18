e68fd92faca4ff1eec68aaaa5035696942a1851b
MarkoPolo01 <kovachevich3001@gmail.com>
2025-12-18 11:06:50 +0300

    add WebView

--- App.tsx
+++ App.tsx
@@ -1,118 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import React, {useRef} from 'react';
+import {ActivityIndicator, SafeAreaView, View} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const webViewRef = useRef<WebView>(null);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const injectJsCode = () => {
+    const token = 'token';
+    return `
+    (function() {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify('${token}')
+      );
+    })();
+  `;
+  };
+
+  const handleMessage = async e => {
+    const message = e.nativeEvent.data;
+
+    switch (message) {
+      case 'refreshToken':
+        // локика по получению токена
+        webViewRef?.current?.injectJavaScript(injectJsCode());
+        break;
+      default:
+        break;
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView style={{flex: 1}}>
+      <WebView
+        source={{uri: 'https://dzen.ru/'}} // нужно заменить на локалхост локально поднятого приложения + добавить логи
+        style={{flex: 1}}
+        ref={webViewRef}
+        thirdPartyCookiesEnabled
+        sharedCookiesEnabled
+        javaScriptEnabled
+        startInLoadingState
+        showsHorizontalScrollIndicator={false}
+        renderLoading={() => (
+          <View style={{flex: 1}}>
+            <ActivityIndicator size="large" />
+          </View>
+        )}
+        mediaPlaybackRequiresUserAction
+        allowsInlineMediaPlayback
+        onError={e => console.log(e)}
+        onHttpError={e => console.log(e)}
+        scrollEnabled
+        allowsBackForwardNavigationGestures
+        injectedJavaScript={injectJsCode()}
+        setSupportMultipleWindows={false}
+        onMessage={handleMessage}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
